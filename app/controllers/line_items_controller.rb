8497004fc72681ef06f89589a12de98903a46b87
tsyan <anna.tsykalova@gmail.com>
2014-02-26 23:47:36 -0500

    added ajax cart refresh

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,9 +1,9 @@
 #---
 # Excerpted from "Agile Web Development with Rails",
 # published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
+# Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
+# We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class LineItemsController < ApplicationController
@@ -39,7 +39,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js
         format.json { render action: 'show',
           status: :created, location: @line_item }
       else
